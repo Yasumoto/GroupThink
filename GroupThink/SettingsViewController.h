0bab271057321021f27439cfdf93ee333e4d4f83
Joe Smith <yasumoto7@gmail.com>
2012-05-16 21:49:54 -0700

    Add a Settings view. Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ GroupThink/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  GroupThink
+//
+//  Created by Joe Smith on 5/16/12.
+//  Copyright (c) 2012 GroupThink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ GroupThink/SettingsViewController.m
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.m
+//  GroupThink
+//
+//  Created by Joe Smith on 5/16/12.
+//  Copyright (c) 2012 GroupThink. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
